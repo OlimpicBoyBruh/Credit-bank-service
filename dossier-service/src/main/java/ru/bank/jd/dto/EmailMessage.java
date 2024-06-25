481e9a0b6253084e4130439baa8d1fe10d93f139
Artem <olimpicboy17@gmail.com>
2024-06-25 13:54:19 +0300

    fix consturct and log error dossier-service

--- dossier-service/src/main/java/ru/bank/jd/dto/EmailMessage.java
+++ dossier-service/src/main/java/ru/bank/jd/dto/EmailMessage.java
@@ -3,10 +3,12 @@ package ru.bank.jd.dto;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import ru.bank.jd.dto.enumerated.Theme;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class EmailMessage {
     @NotBlank(message = "Id не должно быть пустым или null")
     private String statementId;
